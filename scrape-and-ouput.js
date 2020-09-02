77560df8a1e19fd8cc4ad3a51f32b3f4279fa078
Becky Rush <becky.rush@bbc.co.uk>
2020-09-02 08:07:46 +0100

    back up code for use in pipedream

--- scrape-and-ouput.js
+++ scrape-and-ouput.js
@@ -1,48 +1,57 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 
 let browser;
 const pageToScrape = 'https://www.bbc.co.uk/weather/coast-and-sea/tide-tables/8/82';
 const data = {};
 const date = getDate();
-
-async function runTheScripts() {
-    await scrapePages();
-    // await slackAlert();
-}
+let output = '';
 
 async function scrapePages() {
     let webpage = await loadWebpage();
     let pageData = await scrapeResults(webpage);
     console.log('results! ', pageData);
     closeBrowser();
+    output = `Low tide: ${pageData['low'][0]}, ${pageData['low'][1]} \n High tide: ${pageData['high'][0]}`;
 }
 
 // SCRAPING
 
 async function loadWebpage() {
-    browser = await puppeteer.launch({
-        headless: true
+    console.log('load webpage');
+    browser = await puppeteer.connect({
+        browserWSEndpoint: `wss://chrome.browserless.io?token=${process.env.BROWSERLESS_KEY}`
     });
+    console.log('connected');
     page = await browser.newPage();
+    console.log('new page');
     await page.goto(pageToScrape);
+    console.log('on page');
     await page.waitForSelector(".wr-c-tides-table");
+    console.log('waited for selector');
     return page;
 }
 
 async function scrapeResults(page) {
     page.on('console', consoleObj => console.log(consoleObj.text()));
-    const tideTimes = await page.evaluate(({ date, data }) => {
+    const tideTimes = await page.evaluate(({
+        date,
+        data
+    }) => {
         const container = document.querySelector(`#section-${date}`);
         const rows = Array.from(container.querySelectorAll('tbody tr'));
         rows.forEach(row => {
             let tideType = row.querySelector('.wr-c-tide-extremes__type').innerText.trim();
             tideType = tideType.includes('Next') ? tideType.split(':')[1].replace(/^\s+|\s+$/g, '') : tideType;
+            tideType = tideType.toLowerCase();
             const tideTime = row.querySelector('.wr-c-tide-extremes__time').innerText.trim();
             console.log('tide type', tideType, 'tidetime', tideTime);
             data[tideType] = data[tideType] !== undefined ? [...data[tideType], tideTime] : [tideTime];
         });
         return data;
-    }, { date, data });
+    }, {
+        date,
+        data
+    });
     return tideTimes;
 }
 
@@ -72,4 +81,5 @@ const asyncForEach = async (array, callback) => {
 
 // START!
 
-runTheScripts();
+await scrapePages();
+return output;
